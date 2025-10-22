1064b8778d9e79fa9f92959a595feebf9465f79c
unknown <baloghdav2002@gmail.com>
2025-10-22 11:19:14 +0200

    Allowing Vercel

--- backend/main.py
+++ backend/main.py
@@ -13,10 +13,11 @@ logging.basicConfig(level = logging.INFO)
 logger = logging.getLogger(__name__)
 
 app = FastAPI(title = "Allergén kiválasztó")
-
+VERCEL_APP_URL = "https://dev-test-2025-5uqz.vercel.app"
 origins = [
     "http://localhost:3000",
     "http://127.0.0.1:3000",
+    VERCEL_APP_URL,
 ]
 app.add_middleware(
     CORSMiddleware,
